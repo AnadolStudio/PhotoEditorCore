289be5cf5cd5268a1099aec196a5393cb3cbd5c7
Anadol <74777850+AnadolStudio@users.noreply.github.com>
2022-09-17 17:27:40 +0300

    update PhotoEditorView.kt

--- photoeditor/src/main/java/com/anadolstudio/photoeditorcore/view/PhotoEditorView.kt
+++ photoeditor/src/main/java/com/anadolstudio/photoeditorcore/view/PhotoEditorView.kt
@@ -1,12 +1,11 @@
 package com.anadolstudio.photoeditorcore.view
 
 import android.graphics.Bitmap
+import android.widget.ImageView
 import com.anadolstudio.photoeditorcore.domain.edit_processor.EditorView
 
-class PhotoEditorView : EditorView {
+class PhotoEditorView(private val imageView: ImageView) : EditorView {
 
-    override fun setBitmap(bitmap: Bitmap) {
-        TODO("Not yet implemented")
-    }
+    override fun setBitmap(bitmap: Bitmap) = imageView.setImageBitmap(bitmap)
 
 }
